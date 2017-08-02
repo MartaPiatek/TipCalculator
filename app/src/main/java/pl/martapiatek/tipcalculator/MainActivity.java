0dd1cb8edd7ebef0584281151fb0712060af71b1
MartaPiatek <martusia.piatek@gmail.com>
2017-08-02 19:28:33 +0200

    Implementacja metody obliczającej kwotę napiwku i całkowitej należności

--- app/src/main/java/pl/martapiatek/tipcalculator/MainActivity.java
+++ app/src/main/java/pl/martapiatek/tipcalculator/MainActivity.java
@@ -1,13 +1,75 @@
 package pl.martapiatek.tipcalculator;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.NumberFormat;
+
+public class MainActivity extends Activity {
+
+    //obiekty formatujące obiekty walutowe i procentowe
+
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0; // kwota rachunku wprowadzona przez użytkowanika
+    private double percent = 0.15; // początkowy procent napiwku
+    private TextView amountTextView, percentTextView, tipTextView, totalTextView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        tipTextView.setText(currencyFormat.format(0));
+        totalTextView.setText(currencyFormat.format(0));
+
+        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
+
+       // amountEditText.addTextChangedListener(amountEditTextWatcher);
+
+        SeekBar percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
+      //  percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
+    //oblicz i wyświetl kwotę napiwku i całkowitą należność
+    private void calculate(){
+
+        percentTextView.setText(percentFormat.format(percent));
+
+        double tip = billAmount * percent;
+        double total = billAmount + tip;
+
+        tipTextView.setText(currencyFormat.format(tip));
+        totalTextView.setText(currencyFormat.format(total));
+
+
+    }
+
+
 }
